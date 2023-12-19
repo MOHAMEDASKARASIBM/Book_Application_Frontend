462ea0a32a29680e356db1a7b9aad5b22961a6a7
mohamedaskarasibm <mohamedaskarasibm@gmail.com>
2023-12-19 16:58:30 +0530

    update color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4 bg-gray-500'>
+    <div className='p-4 bg-gray-600'>
       <div className='flex justify-center items-center gap-x-4'>
         <button
           className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
